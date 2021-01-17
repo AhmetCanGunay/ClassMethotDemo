3baeb62a6d91cc315db5c41799607899e69020bb
Ahmet Can GÜNAY <ahmetcangunay@outlook.com>
2021-01-17 21:51:10 +0300

    Proje dosyası ekle.

---
+++ ClassMethotDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriSorgula(Musteri musteri)
+        {
+            Console.WriteLine("Ad:{0}\nSoyadı:{1}\nMüşteri No:{2}",musteri.MusteriAdi,musteri.MusteriSoyadi,musteri.MusteriNo);
+        }
+        public void MüsteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Başarıyla Eklendi! " + musteri.MusteriAdi+" "+musteri.MusteriSoyadi);
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri({0}) Başarıyla Silinmiştir!",musteri.MusteriAdi);
+        }
+        
+    }
+}
---
+++ ClassMethotDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassMethotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.MusteriAdi = "Ömer";
+            musteri1.MusteriSoyadi = "PARLAK";
+            musteri1.MusteriNo = "1234567";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.MusteriAdi = "Ahmet Can";
+            musteri2.MusteriSoyadi = "GÜNAY";
+            musteri2.MusteriNo = "7654321";
+
+            Musteri[] MusteriList = new Musteri[] { musteri1, musteri2 };
+            MusteriManager customer = new MusteriManager();
+            Console.WriteLine("Bankamıza Hoşgeldiniz Sayın {0}",musteri1.MusteriAdi+" "+musteri1.MusteriSoyadi);
+            Console.WriteLine("Lütfen Aşağıdaki İşlemlerden Birini Seçiniz:\n1)Müşteri Sorgula\n2)Müşteri Ekle\n3)Müşteri Sil");
+            int Islem=Convert.ToInt32(Console.ReadLine());
+            if (Islem==1)
+            {
+                customer.MusteriSorgula(musteri1);
+            }
+            else if (Islem==2)
+            {
+                customer.MüsteriEkle(musteri1);
+            }
+            else
+            {
+                customer.MusteriSil(musteri1);
+            }
+        }
+    }
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string MusteriAdi { get; set; }
+        public string MusteriSoyadi { get; set; }
+        public string MusteriNo { get; set; }
+
+    }
+}
